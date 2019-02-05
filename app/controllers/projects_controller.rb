0b3bdfd7b55a82a3ba97ab7c1d351f51a24e03e3
Francesco Stefani <francesco.stefani@outlook.com>
2019-02-05 01:00:50 +0000

    changes-in-controller-for-papers-and-projects-to-get-the-query

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,162 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, only: [:show, :edit, :update, :destroy, :replies_project]
 
+  def replies_project
+    # @replies = Question.where("project_id = ? AND paper_id = ?", params[:project_id], params[:paper_id])
+
+    # all_question_1 = []
+
+    # @replies.map do |reply|
+    #   all_question_1 << reply.question_1.to_i
+    # end
+
+    # na_question_1 = all_question_1.count(-1)
+    # no_question_1 = all_question_1.count(0)
+    # maybe_question_1 = all_question_1.count(0.5)
+    # yes_question_1 = all_question_1.count(1)
+
+    # len_question_1 = all_question_1.length
+
+    # stat_na_question_1 = na_question_1 / len_question_1
+    @replies_project = Question.where("project_id = ?", params[:project_id])
+
+    @replies_project_total = Question.where("project_id = ?", params[:project_id]).length
+
+    @replies_project_NA_question_1 = Question.where("project_id = ? AND question_1 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_1 = Question.where("project_id = ? AND question_1 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_1 = Question.where("project_id = ? AND question_1 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_1 = Question.where("project_id = ? AND question_1 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_2 = Question.where("project_id = ? AND question_2 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_2 = Question.where("project_id = ? AND question_2 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_2 = Question.where("project_id = ? AND question_2 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_2 = Question.where("project_id = ? AND question_2 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_3 = Question.where("project_id = ? AND question_3 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_3 = Question.where("project_id = ? AND question_3 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_3 = Question.where("project_id = ? AND question_3 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_3 = Question.where("project_id = ? AND question_3 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_4 = Question.where("project_id = ? AND question_4 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_4 = Question.where("project_id = ? AND question_4 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_4 = Question.where("project_id = ? AND question_4 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_4 = Question.where("project_id = ? AND question_4 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_5 = Question.where("project_id = ? AND question_5 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_5 = Question.where("project_id = ? AND question_5 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_5 = Question.where("project_id = ? AND question_5 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_5 = Question.where("project_id = ? AND question_5 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_6 = Question.where("project_id = ? AND question_6 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_6 = Question.where("project_id = ? AND question_6 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_6 = Question.where("project_id = ? AND question_6 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_6 = Question.where("project_id = ? AND question_6 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_7 = Question.where("project_id = ? AND question_7 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_7 = Question.where("project_id = ? AND question_7 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_7 = Question.where("project_id = ? AND question_7 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_7 = Question.where("project_id = ? AND question_7 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_8 = Question.where("project_id = ? AND question_8 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_8 = Question.where("project_id = ? AND question_8 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_8 = Question.where("project_id = ? AND question_8 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_8 = Question.where("project_id = ? AND question_8 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_9 = Question.where("project_id = ? AND question_9 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_9 = Question.where("project_id = ? AND question_9 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_9 = Question.where("project_id = ? AND question_9 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_9 = Question.where("project_id = ? AND question_9 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_10 = Question.where("project_id = ? AND question_10 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_10 = Question.where("project_id = ? AND question_10 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_10 = Question.where("project_id = ? AND question_10 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_10 = Question.where("project_id = ? AND question_10 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_11 = Question.where("project_id = ? AND question_11 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_11 = Question.where("project_id = ? AND question_11 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_11 = Question.where("project_id = ? AND question_11 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_11 = Question.where("project_id = ? AND question_11 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_12 = Question.where("project_id = ? AND question_13 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_12 = Question.where("project_id = ? AND question_13 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_12 = Question.where("project_id = ? AND question_13 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_12 = Question.where("project_id = ? AND question_13 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_14 = Question.where("project_id = ? AND question_14 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_14 = Question.where("project_id = ? AND question_14 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_14 = Question.where("project_id = ? AND question_14 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_14 = Question.where("project_id = ? AND question_14 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_15 = Question.where("project_id = ? AND question_15 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_15 = Question.where("project_id = ? AND question_15 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_15 = Question.where("project_id = ? AND question_15 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_15 = Question.where("project_id = ? AND question_15 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_16 = Question.where("project_id = ? AND question_16 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_16 = Question.where("project_id = ? AND question_16 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_16 = Question.where("project_id = ? AND question_16 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_16 = Question.where("project_id = ? AND question_16 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_17 = Question.where("project_id = ? AND question_17 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_17 = Question.where("project_id = ? AND question_17 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_17 = Question.where("project_id = ? AND question_17 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_17 = Question.where("project_id = ? AND question_17 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_18 = Question.where("project_id = ? AND question_18 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_18 = Question.where("project_id = ? AND question_18 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_18 = Question.where("project_id = ? AND question_18 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_18 = Question.where("project_id = ? AND question_18 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_19 = Question.where("project_id = ? AND question_19 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_19 = Question.where("project_id = ? AND question_19 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_19 = Question.where("project_id = ? AND question_19 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_19 = Question.where("project_id = ? AND question_19 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_20 = Question.where("project_id = ? AND question_20 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_20 = Question.where("project_id = ? AND question_20 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_20 = Question.where("project_id = ? AND question_20 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_20 = Question.where("project_id = ? AND question_20 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_21 = Question.where("project_id = ? AND question_21 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_21 = Question.where("project_id = ? AND question_21 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_21 = Question.where("project_id = ? AND question_21 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_21 = Question.where("project_id = ? AND question_21 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_22 = Question.where("project_id = ? AND question_22 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_22 = Question.where("project_id = ? AND question_22 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_22 = Question.where("project_id = ? AND question_22 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_22 = Question.where("project_id = ? AND question_22 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_23 = Question.where("project_id = ? AND question_23 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_23 = Question.where("project_id = ? AND question_23 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_23 = Question.where("project_id = ? AND question_23 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_23 = Question.where("project_id = ? AND question_23 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_24 = Question.where("project_id = ? AND question_24 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_24 = Question.where("project_id = ? AND question_24 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_24 = Question.where("project_id = ? AND question_24 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_24 = Question.where("project_id = ? AND question_24 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_25 = Question.where("project_id = ? AND question_25 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_25 = Question.where("project_id = ? AND question_25 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_25 = Question.where("project_id = ? AND question_25 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_25 = Question.where("project_id = ? AND question_25 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_26 = Question.where("project_id = ? AND question_26 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_26 = Question.where("project_id = ? AND question_26 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_26 = Question.where("project_id = ? AND question_26 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_26 = Question.where("project_id = ? AND question_26 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_27 = Question.where("project_id = ? AND question_27 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_27 = Question.where("project_id = ? AND question_27 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_27 = Question.where("project_id = ? AND question_27 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_27 = Question.where("project_id = ? AND question_27 = ?", params[:project_id], "1").length
+
+    @replies_project_NA_question_28 = Question.where("project_id = ? AND question_28 = ?", params[:project_id], "-1").length
+    @replies_project_NO_question_28 = Question.where("project_id = ? AND question_28 = ?", params[:project_id], "0").length
+    @replies_project_MAYBE_question_28 = Question.where("project_id = ? AND question_28 = ?", params[:project_id], "0.5").length
+    @replies_project_YES_question_28 = Question.where("project_id = ? AND question_28 = ?", params[:project_id], "1").length
+  end
 
   def index
     @projects = Project.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 Rails.application.routes.draw do
   resources :projects do
+    member do
+      get "replies_project"
+    end
     resources :papers do
-        get :statistics
       resources :questions do
-        get :replies
+        collection do
+          get 'replies'
+        end
       end
     end
   end
-  devise_for :users
-  root to: 'pages#home'
+
+devise_for :users
+root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# resources :questions do
+#   collection do
+#     get 'replies'
+#   end
+# end
