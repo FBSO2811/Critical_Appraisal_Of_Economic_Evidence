388c2b607cc8016e57cddf07308778aaa37b6b96
Francesco Stefani <francesco.stefani@outlook.com>
2019-03-23 12:53:28 +0000

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -185,7 +185,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-
+end
 
 # ---------------------
 
